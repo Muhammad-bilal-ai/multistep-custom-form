4f7d93f92776137c70b1d130435d5f44953b5cb2
Muhammad Bilal <withbilal777@gmail.com>
2024-06-29 13:04:28 +0500

    multistep form is saving data on firestore

--- components/firebase.js
+++ components/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,8 +19,9 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
-export { db };
+export { db, storage };
 
 import { doc, setDoc } from "firebase/firestore";
 
